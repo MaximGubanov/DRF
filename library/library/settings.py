0772412ed32805dbcd0d8f542035d2951c82f89d
MaximGubanov <m_gubanov@bk.ru>
2022-04-27 14:21:39 +0300

    Урок_2._Введение_в_React._Компонентный_подход

--- library/library/settings.py
+++ library/library/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'userapp.User'
 
 # Application definition
 
@@ -40,9 +41,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'mainapp',
     'userapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
--- library/mainapp/models.py
+++ library/mainapp/models.py
@@ -6,4 +6,5 @@ class Author(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid4)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
